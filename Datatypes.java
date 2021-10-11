9495de4b1b5a6ae5b047a9bee75ec8d563d80745
Abid hussain <35552293+abid888888@users.noreply.github.com>
2021-10-12 00:30:38 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,32 @@
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte my_age = 22;
+		short my_salary = 10000;
+		int pincode = 766445;
+		long my_phone = 8247457420L;
+		
+		float percentage = 93.68f;
+		double annualIncome = 86446788.754d;
+		
+		boolean male = true;
+		char letter = 'A';
+		
+		String my_name = " Abid";
+		
+		System.out.println("byte : "+my_age);
+		System.out.println("short : "+my_salary);
+		System.out.println("int : "+pincode);
+		System.out.println("long : "+my_phone);
+		System.out.println("float : "+percentage);
+		System.out.println("double : "+annualIncome);
+		System.out.println("boolean : "+male);
+		System.out.println("char : "+letter);
+		System.out.println("String : "+my_name);
+		
+	}
+
+}
