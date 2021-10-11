f8c6e22bc65b1e94bd6f6112ae10c2fe7d108b17
Abid hussain <35552293+abid888888@users.noreply.github.com>
2021-10-12 00:24:10 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,45 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		
+		int num1 = 100;
+		int num2 = 30;
+		int num3 = 50;
+		
+		if(num1 > num2 && num1 > num3) {
+			System.out.println(num1);
+		}
+		else if(num2 > num1 && num2 > num3) {
+			System.out.println(num2);
+		}
+		else {
+			System.out.println(num3);
+		}
+		
+		
+		
+		
+		int my_num1 = 10;
+		System.out.println(my_num1);
+		my_num1 += 5;
+		System.out.println(my_num1);
+		
+		int my_num2 = 100;
+		System.out.println(my_num2);
+		my_num2 /= 5;
+		System.out.println(my_num2);
+		
+		int my_num3 = 50;
+		System.out.println(my_num3);
+		my_num3 *= 5;
+		System.out.println(my_num3);
+		
+		int my_num4 = 30;
+		System.out.println(my_num4);
+		my_num4 -= 5;
+		System.out.println(my_num4);
+
+	}
+
+}
---
+++ Sum.java
@@ -0,0 +1,15 @@
+
+public class Sum {
+	
+	public static void main(String args[]) {
+		
+		int sum = 0;
+		for(int i = 0; i <= 50; i++) {
+			sum += i;
+		}
+		
+		System.out.println(sum);
+		
+	}
+
+}
